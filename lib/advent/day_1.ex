f4e8f94a4daa82b45e4ab76f287543cec25ab313
Joseph Lozano <joseph@joseph-lozano.com>
2019-12-06 11:07:48 -0500

    part 2

--- lib/advent/day_1.ex
+++ lib/advent/day_1.ex
@@ -1,24 +1,37 @@
 defmodule Advent.Day1 do
-  @spec execute() :: integer
-  def execute() do
+  @spec execute(:part_1 | :part_2) :: integer
+  def execute(part) do
     File.read!("inputs/day_1.txt")
-    |> execute
+    |> execute(part)
   end
 
-  @spec execute(binary | integer) :: integer
-  def execute(input) when is_binary(input) do
+  @spec execute(binary() | integer()) :: integer
+  def execute(input, part) when is_binary(input) do
     input
     |> String.split("\n")
     |> Enum.reduce(0, fn el, acc ->
       Integer.parse(el)
       |> case do
         :error -> acc
-        {num, _} -> acc + execute(num)
+        {num, _} -> acc + execute(num, part)
       end
     end)
   end
 
-  def execute(input) when is_integer(input) do
+  def execute(num, :part_1), do: part_1(num)
+  def execute(num, :part_2), do: part_2(num)
+
+  def part_1(input) when is_integer(input) do
     floor(input / 3) - 2
   end
+
+  @spec part_2(integer()) :: integer
+
+  def part_2(input) when is_integer(input) and input > 0 do
+    sum = floor(input / 3) - 2
+    sum = if sum < 0, do: 0, else: sum
+    sum + part_2(sum)
+  end
+
+  def part_2(input) when is_integer(input), do: 0
 end
--- test/advent/day_1_test.exs
+++ test/advent/day_1_test.exs
@@ -3,10 +3,16 @@ defmodule Advent.Day1Test do
 
   alias Advent.Day1
 
-  test("Day 1") do
-    assert Day1.execute(12) == 2
-    assert Day1.execute(14) == 2
-    assert Day1.execute(1969) == 654
-    assert Day1.execute(100_756) == 33583
+  test("part 1") do
+    assert Day1.part_1(12) == 2
+    assert Day1.part_1(14) == 2
+    assert Day1.part_1(1969) == 654
+    assert Day1.part_1(100_756) == 33583
+  end
+
+  test("part 2") do
+    assert Day1.part_2(12) == 2
+    assert Day1.part_2(1969) == 966
+    assert Day1.part_2(100_756) == 50346
   end
 end
