35542e7063727d680560c2b5a895a57f8fd770a9
Joseph Lozano <joseph@joseph-lozano.com>
2019-12-06 13:04:41 -0500

    describe block

--- test/advent/day_1_test.exs
+++ test/advent/day_1_test.exs
@@ -3,16 +3,18 @@ defmodule Advent.Day1Test do
 
   alias Advent.Day1
 
-  test("part 1") do
-    assert Day1.part_1(12) == 2
-    assert Day1.part_1(14) == 2
-    assert Day1.part_1(1969) == 654
-    assert Day1.part_1(100_756) == 33583
-  end
+  describe "day 1" do
+    test("part 1") do
+      assert Day1.part_1(12) == 2
+      assert Day1.part_1(14) == 2
+      assert Day1.part_1(1969) == 654
+      assert Day1.part_1(100_756) == 33583
+    end
 
-  test("part 2") do
-    assert Day1.part_2(12) == 2
-    assert Day1.part_2(1969) == 966
-    assert Day1.part_2(100_756) == 50346
+    test("part 2") do
+      assert Day1.part_2(12) == 2
+      assert Day1.part_2(1969) == 966
+      assert Day1.part_2(100_756) == 50346
+    end
   end
 end
